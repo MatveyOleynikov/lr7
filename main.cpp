feee31db72c2db46a07cefa7d81e3917d0d45723
MatveyOleynikov <ochoba-1@mail.ru>
2023-05-17 17:37:23 +0300

    min, max, swap 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+void swp(int& a, int& b){ //1
+    int tmp = a; //1
+    a = b; //1
+    b = tmp; //1
+} //1
+
+int mn(int& a, int& b){ //1
+    if (a < b){ //1
+        return a; //1
+    } //1
+    return b; //1
+} //1
+
+int mx(int& a, int& b){ //1
+    if (a > b){ //1
+        return a; //1
+    } //1
+    return b; //1
+} //1
+
 int main(){
-    
+
     return 0;
 }
