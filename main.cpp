fa79cccd20680229b6341e04990db5f9c244c0bc
MatveyOleynikov <ochoba-1@mail.ru>
2023-05-17 17:38:05 +0300

    template min, max 2 and rvalue-ref min with = 7

--- main.cpp
+++ main.cpp
@@ -6,19 +6,29 @@ void swp(int& a, int& b){ //1
     b = tmp; //1
 } //1
 
-int mn(int& a, int& b){ //1
+template <typename T> //2
+T mn(T& a, T& b){ //1, 2
     if (a < b){ //1
         return a; //1
     } //1
     return b; //1
 } //1
 
-int mx(int& a, int& b){ //1
+template <typename T> //2
+T mx(T& a, T& b){ //1, 2
     if (a > b){ //1
         return a; //1
     } //1
     return b; //1
 } //1
+
+template <typename T> //5
+T&& mn(T&& a, T&& b){ //5
+    if (a <= b){ //5, 7
+        return std::move(a); //5, 7
+    } //5
+    return std::move(b); //5, 7
+} //5
 
 int main(){
 
