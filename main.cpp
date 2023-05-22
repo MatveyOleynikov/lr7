2983ca8a0893a97d08b980b6f897bfb46606a35b
MatveyOleynikov <ochoba-1@mail.ru>
2023-05-22 12:57:25 +0300

    made header 5.1

--- main.cpp
+++ main.cpp
@@ -24,10 +24,6 @@ T mx(T& a, T& b){ //1, 2
 
 template <typename T> //5
 T&& mn(T&& a, T&& b){ //5
-    if (a <= b){ //5, 7
-        return std::move(a); //5, 7
-    } //5
-    return std::move(b); //5, 7
 } //5
 
 int main(){
